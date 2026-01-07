986e35c85ca4b7be2e26dff7a36e91ac93179483
KodhaiTikkaram <kodhai0612@gmail.com>
2026-01-07 16:57:31 +0530

    Create PROBLEM4.py

---
+++ PROBLEM4.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the Expenses:"));
+for i in range(a):
+    amount=int(input())
+    category=input()
+    print(amount,category)
+for y in range(a):
+    y=y+x
+print("Total Amount:",y)
