f2c6de46fd0d67b2ba67797bcf19e2a583b12a7d
KodhaiTikkaram <kodhai0612@gmail.com>
2026-01-07 16:47:54 +0530

    Update Password.py

--- PROBLEM6/Password.py
+++ PROBLEM6/Password.py
@@ -1,7 +1,11 @@
 x=input("Enter Password:")
-if x.isdigit and x.isupper and x.islower :
+a=x.isupper
+b=x.islower
+c=x.isalnum
+e=x.isnumeric
+if (len(x)>8 and a)and(b and c and e):
     print("Password Strength:Strong")
-elif x.isdigit or x.isalpha:
+elif b or e:
     print("Password Strength:Medium")
 else:
     print("Password Strength:Weak")
